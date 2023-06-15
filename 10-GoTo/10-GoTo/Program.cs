2c8489a633eb01e043a1c1a52442f89969580f5d
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-06-14 21:25:52 -0300

    Atualização

--- 10-GoTo/10-GoTo/Program.cs
+++ 10-GoTo/10-GoTo/Program.cs
@@ -10,6 +10,57 @@ namespace _10_GoTo
     {
         static void Main(string[] args)
         {
+            int tempo = 0;
+            char escolha;
+
+            inicio:
+
+            Console.Clear();
+
+            Console.WriteLine("Belo Horizonte/MG a Vitoria/ES");
+            Console.WriteLine("Escolha o transporte: [a] Avião [c] Carro [o] Ônibus ");
+            escolha = char.Parse(Console.ReadLine());
+            switch (escolha)
+            {
+                case 'a':
+                case 'A':
+                    tempo = 50;
+                    break;
+                case 'c':
+                case 'C':
+                    tempo = 480;
+                    break;
+                case 'o':
+                case 'O':
+                    tempo = 660;
+                    break;
+                default:
+                    tempo = -1;
+                    break;
+            }
+            if (tempo < 0)
+            {
+                Console.WriteLine("Transporte indisponivel");
+            }
+            else
+            {
+                Console.WriteLine("Para o transporte escolhido o tempo é: {0} minutos", tempo);
+            }
+
+            Console.WriteLine("Calcular outro transporte? [s/n]");
+            escolha = char.Parse(Console.ReadLine());
+
+            if(escolha == 's' || escolha == 'S')
+            {
+                goto inicio;
+            }
+            else
+            {
+                Console.Clear() ;
+                Console.WriteLine("Fim do programa");
+                Console.ReadLine();
+            }
+            
         }
     }
 }
---
+++ 11-Array/11-Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n1,n2,n3,n4,n5;
+            int[] n = new int[5];
+            n[0] = 11;
+            n[1] = 22;
+            n[2] = 33;
+            n[3] = 44;
+            n[4] = 55;
+            Console.WriteLine(n[0]);
+            Console.ReadLine();
+
+            int[] num = new int[3] {11,22,33};
+            Console.WriteLine(num[1]);
+            Console.ReadLine();
+
+            int[] numero = { 11, 22, 33 }; //o tamanho do array é definido pela quantidade de elementos 
+            Console.WriteLine(numero[2]);
+            Console.ReadLine();
+        }
+    }
+}
