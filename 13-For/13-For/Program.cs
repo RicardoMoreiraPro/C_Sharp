85540cd75e0f6881426b5fa64a10ccceb818998c
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-06-14 23:21:47 -0300

    Atualização

---
+++ 13-For/13-For/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int num=0;num<10;num++)
+            {
+                Console.WriteLine(num + "º repetição");
+            }
+            Console.ReadLine();
+
+            int[] n = new int[10];
+            for (int i = 0; i < n.Length; i++)
+            {
+                n[i] = i*2;
+                Console.WriteLine("Valor da n na pos {0}: {1}", i, n[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 14-while/14-while/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            int i = 0;
+            while (i<num.Length)
+            {
+                num[i] = i;
+                Console.WriteLine(num[i]);
+                i++;                
+            }
+
+            Console.WriteLine("Fim");
+            Console.ReadLine();
+
+        }
+    }
+}
