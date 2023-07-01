e5028a9c1209e33e4c4b19845641fa43628dfaf7
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-07-01 09:16:05 -0300

    Atualização

---
+++ 15-Do While/15-Do While/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123";
+            string senhauser;
+            int tentativas = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a senha");
+                senhauser = Console.ReadLine();
+                tentativas++;
+            }while(senha != senhauser);
+            Console.WriteLine("Senha correta!\nTentativas: {0}", tentativas);
+            Console.ReadLine();
+
+
+            int num = 5;
+            while (num < 5)
+            {
+                Console.WriteLine("Ricardo1");
+            }
+
+            do{
+                Console.WriteLine("Ricardo2");
+            }while (num < 5);
+
+            Console.ReadLine();
+        }
+    }
+}
