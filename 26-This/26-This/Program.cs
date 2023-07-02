397e83a49ae05a240ef77b590beabf919b44a3cb
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-07-01 23:10:33 -0300

    Atualização

---
+++ 26-This/26-This/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Calculos
+{
+    public int v1;
+    public int v2;
+
+    public Calculos(int v1, int v2)
+    {
+        this.v1 = v1;
+        this.v2 = v2;
+        Console.WriteLine("{0} e {1}", v1, v2);
+    }
+
+    public int Somar()
+    {
+        Console.WriteLine("{0} e {1}", v1, v2);
+        return v1 + v2;
+    }
+}
+
+namespace _26_This
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculos c = new Calculos(10, 2);
+            Calculos d = new Calculos(5, 1);
+            int b = c.Somar();
+            int e = d.Somar();
+            Console.WriteLine(b);
+            Console.WriteLine(e);
+            Console.ReadLine();
+        }
+    }
+}
