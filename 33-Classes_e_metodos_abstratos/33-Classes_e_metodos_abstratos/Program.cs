f6c231fbab77246f070cad11ac191f27af633910
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-07-04 23:18:20 -0300

    Atualização

---
+++ 33-Classes_e_metodos_abstratos/33-Classes_e_metodos_abstratos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+abstract class Veiculo
+{
+    protected int velMaxima;
+    protected int velAtual;
+    protected bool ligado;
+    public Veiculo()
+    {
+        ligado = false;
+        velAtual = 0;
+    }
+    public void setLigado(bool ligado)
+    {
+        this.ligado = ligado;
+    }
+    public int getVelAtual()
+    {
+        return velAtual;
+    }
+    abstract public void aceleracao(int mult);
+}
+class Carro : Veiculo
+{
+    public Carro()
+    {
+        velMaxima = 120;
+    }
+    override public void aceleracao(int mult)
+    {
+        velAtual +=10* mult;
+    }
+    
+}
+
+namespace _33_Classes_e_metodos_abstratos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro1 = new Carro();
+            carro1.aceleracao(1);
+            Console.WriteLine(carro1.getVelAtual());
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 34-Classe_sealed/34-Classe_sealed/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+sealed class Veiculo  //Não pode ser herdada
+{
+
+}
+
+namespace _34_Classe_sealed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
