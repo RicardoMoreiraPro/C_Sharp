4f025f57cdbb4abd777999258e139c10a6404f3c
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-07-09 17:39:57 -0300

    Atualização

---
+++ 35-Get_e_Set/35-Get_e_Set/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Carro
+{
+    private int velMax;
+    public int vm
+    {
+        get
+        {
+            return velMax;
+        }
+        set
+        {
+            if(value < 0)
+            {
+                velMax = 0;
+            }else if (value > 300)
+            {
+                velMax = 300;
+            }else
+            {
+                velMax = value;
+            }
+        }
+    }
+    public Carro()
+    {
+        vm = 120;
+    }
+}
+
+namespace _35_Get_e_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = new Carro();
+            c1.vm = 200;
+            Console.WriteLine("Velocidade:{0}",c1.vm);
+            Console.ReadLine();
+        }
+    }
+}
