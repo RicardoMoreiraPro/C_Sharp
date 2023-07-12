f6f4b0a77cad806a102e4cdc5aacf3481519c013
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-07-11 22:54:37 -0300

    Atualização

---
+++ 49-Coleção_dictionary/49-Coleção_dictionary/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _49_Coleção_dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary <int, string> //int = chave  string = valor/elemento
+            Veiculos = new Dictionary<int, string> ();
+            Veiculos.Add(10, "Carro");
+            Veiculos.Add(5, "Avião");
+            Veiculos.Add(0, "Navio");
+            Veiculos.Add(20, "Moto");
+            Veiculos.Add(15, "Patinete");
+
+            //Veiculos.Clear();   para limpar o dictionary
+            Veiculos.Remove(10);   //Para remover um valor
+            Console.WriteLine("Tamanho do Dictionary: {0}", Veiculos.Count); //retorna quantos valores há na coleção
+            int chave = 20;
+            if (Veiculos.ContainsKey(chave))  //verifica se a chave está na coleção
+            {
+                Console.WriteLine("A chave {0} está na coleção", chave);
+            }
+            else
+            {
+                Console.WriteLine("A chave {0} não está na coleção", chave);
+            }
+
+            string valor = "Navio";
+            if (Veiculos.ContainsValue(valor))  //verifica se o valor está na coleção
+            {
+                Console.WriteLine("O valor {0} está na coleção", valor);
+            }
+            else
+            {
+                Console.WriteLine("O valor {0} não está na coleção", valor);
+            }
+            Veiculos[15] = "Bicicleta";     //Para alterar um valor
+            Console.WriteLine(Veiculos[15]);
+            Console.WriteLine("-------------------");
+
+            //Dictionary<int, string>.ValueCollection valores = Veiculos.Values; ou abaixo KeyValuePair
+            foreach(KeyValuePair<int,string> v in Veiculos)
+            {
+                Console.WriteLine (v);   //(v.Value) ou (v.Key) para imprimir somente o valor ou chave
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 50-Coleção_LinkedList/50-Coleção_LinkedList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50_Coleção_LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> transp = new LinkedList<string>();
+
+            transp.AddFirst("Carro");   //adiciona no inicio da lista
+            transp.AddFirst("Aviao");
+            transp.AddFirst("Navio");
+            transp.AddFirst("Motocicleta");
+            transp.AddLast("Bicicleta");    //adiciona no final da lista
+            LinkedListNode<string>no;
+            no = transp.FindLast("Navio");
+            transp.AddAfter(no,"Patinete");     //adiciona após de um determinado elemento
+            transp.AddBefore(no, "Patins");     //adiciona antes de um determinado elemento
+            //transp.Clear();  //remove todos os elementos
+            
+            foreach (string t in transp)
+            {
+                Console.WriteLine("Transporte: {0}",t);
+            }
+
+            if (transp.Find("Carro") == null)   //verifica se o valor há na lista
+            {
+                Console.WriteLine("Não encontrado");
+            }
+            else
+            {
+                Console.WriteLine("O elemento foi encontrado");
+            }
+            transp.Remove("Navio");    //Para remover um elemento
+            transp.RemoveFirst();  //Remove o primeiro elemento
+            transp.RemoveLast();   //Remove o ultimo elemento
+            foreach (string t in transp)
+            {
+                Console.WriteLine("Transporte: {0}", t);
+            }
+            Console.ReadLine();
+        }
+    }
+}
