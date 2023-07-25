714ad7ed2836dd4a27bff648a4ced0b300196b57
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-07-24 23:17:15 -0300

    Atualização

---
+++ 57_Projeto_Academia/Banco.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _57_Projeto_Academia
+{
+    internal class Banco
+    {
+        private static SQLiteConnection conexao;
+
+        private static SQLiteConnection ConexaoBanco()
+        {
+            conexao = new SQLiteConnection("Data Source = C:\\Users\\ricar\\OneDrive\\Ambiente de Trabalho\\Rep.GitHub\\C#\\57_Projeto_Academia\\Banco\\banco");
+            conexao.Open();
+            return conexao;
+        }
+        public static DataTable ObterTodosUsuarios()
+        {
+            SQLiteDataAdapter da = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                using (var cmd = ConexaoBanco().CreateCommand())
+                {
+                    cmd.CommandText = "Select * from tb_usuarios";
+                    da = new SQLiteDataAdapter(cmd.CommandText, ConexaoBanco());
+                    da.Fill(dt);
+                    ConexaoBanco().Close();
+                    return dt;
+                }
+            }
+            catch(Exception ex)
+            {
+                ConexaoBanco().Close();
+                throw ex;
+            }
+        }
+        public static DataTable consulta(string sql)
+        {
+            SQLiteDataAdapter da = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                using (var cmd = ConexaoBanco().CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    da = new SQLiteDataAdapter(cmd.CommandText, ConexaoBanco());
+                    da.Fill(dt);
+                    ConexaoBanco().Close();
+                    return dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                ConexaoBanco().Close();
+                throw ex;
+            }
+        }
+    }
+}
---
+++ 57_Projeto_Academia/F_Login.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _57_Projeto_Academia
+{
+    public partial class F_Login : Form
+    {
+        Form1 form1;
+        DataTable dt = new DataTable();
+        public F_Login(Form1 f)
+        {
+            InitializeComponent();
+            form1 = f;
+        }
+
+        private void btn_logar_Click(object sender, EventArgs e)
+        {
+            string username = tb_userName.Text;
+            string senha = tb_senha.Text;
+            if (username == "" || senha == "")
+            {
+                MessageBox.Show("Usuario e ou senha inválidos");
+                tb_userName.Focus();
+                return;
+            }
+            string sql = "select * from tb_usuarios where t_username ='" + username + "'and t_senhausuario='" + senha + "'";
+            dt = Banco.consulta(sql);
+            if(dt.Rows.Count == 1)
+            {
+                form1.lb_acesso.Text = dt.Rows[0].ItemArray[5].ToString();
+                form1.lb_nomeUsuario.Text = dt.Rows[0].Field<string>("T_NOMEUSUARIO");
+                form1.pb_ledLogado.Image = Properties.Resources.led_verde;
+                Globais.nivel = int.Parse(dt.Rows[0].Field<Int64>("N_NIVELUSUARIO").ToString());
+                Globais.logado = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario não encontrado!");
+            }
+        }
+
+        private void btn_cancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
