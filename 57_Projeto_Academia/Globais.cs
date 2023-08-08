24c3ac83653b319e56bf73a6c2284478b344286d
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-08-08 20:50:34 -0300

    Atualização

--- 57_Projeto_Academia/Globais.cs
+++ 57_Projeto_Academia/Globais.cs
@@ -11,7 +11,8 @@ namespace _57_Projeto_Academia
         public static string versao = "1.0";
         public static bool logado = false;
         public static int nivel = 0;  //0=Basico   1=Gerente   2=Master
-        public static string caminho = System.Environment.CurrentDirectory;
+        //public static string caminho = System.Environment.CurrentDirectory;
+        public static string caminho = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
         public static string nomeBanco = "banco_academia.db";
         public static string caminhoBanco = caminho + @"\Banco\";
         public static string caminhoFotos = caminho + @"\foto\";
